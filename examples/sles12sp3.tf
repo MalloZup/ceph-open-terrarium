5553e9e76d882f925d5bc468a532f5dda157b3d6
dmaiocchi <dmaiocchi@suse.com>
2018-10-10 10:47:51 +0200

    Add sles jeos images

--- examples/sles12sp3.tf
+++ examples/sles12sp3.tf
@@ -6,21 +6,21 @@ module "cloudinit" {
   source = "./terraform/libvirt/images/cloudinit"
 }
 
-module "sles" {
+module "opensuse" {
   source = "./terraform/libvirt/images/sles/"
 }
 
 // we create 4 hosts 
 
-resource "libvirt_volume" "sles12sp3_disk" {
+resource "libvirt_volume" "sles_disk" {
   name = "sles12sp3-${count.index}"
-  base_volume_id = "${module.sles.sles_12_sp3_id}"
+  base_volume_id = "${module.sles.opensuse_423_id}"
   pool = "default"
   count = 1
 }
 
-resource "libvirt_domain" "sles12sp3" {
-  name = "sles12sp3-${count.index}"
+resource "libvirt_domain" "opensuse423" {
+  name = "opensuse423-${count.index}"
   memory = "1024"
   vcpu = 1
   count = 1
@@ -30,7 +30,7 @@ resource "libvirt_domain" "sles12sp3" {
   }
 
   disk  {
-      volume_id = "${element(libvirt_volume.sles12sp3_disk.*.id, count.index)}"
+      volume_id = "${element(libvirt_volume.opensuse_disk.*.id, count.index)}"
    }
 
 }
---
+++ terraform/libvirt/images/sles/outputs.tf
@@ -0,0 +1,6 @@
+output "sles_12_sp3_id" {
+  value = "${libvirt_volume.sles_12_sp3.id}"
+}
+output "sles_15_id" {
+  value = "${libvirt_volume.sles_15.id}"
+}

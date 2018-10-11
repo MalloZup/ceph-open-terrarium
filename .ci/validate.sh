e2b1647392d4f90947b0bdfb6bea7d3e8fd300e9
dmaiocchi <dmaiocchi@suse.com>
2018-10-11 22:56:02 +0200

    add firt ci

--- .ci/validate.sh
+++ .ci/validate.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
+# we call this script inside the .ci dir
+
 set -e
+
+cd ..
+if [ `terraform fmt | wc -c` -ne 0 ]; then echo "terraform files need be formatted! run terraform fmt!"; exit 1; fi
+cd .ci/
 
 cd ../examples
 for file in "."/*

93864f7b138c6caa642541caa9024b2b0482abf2
dmaiocchi <dmaiocchi@suse.com>
2018-10-11 18:24:56 +0200

    ASS implementation of dinamic host script

--- salt/create_roster.py
+++ salt/create_roster.py
@@ -2,15 +2,39 @@
 
 # this simple script take the hosts.txt files and convert the ip dinamically to salt-ssh roster.
 # this is a workaround until https://github.com/MalloZup/ceph-open-terrarium/issues/7
-
-ips = [12, 34, 45, 69, 12]
+# there is no much need to make it better since it will removed.
+import os
 roster_file = "etc/salt/roster"
+terraformHostsFile = "../hosts.txt"
+
+def readIpfromTerraform(hostFile):
+    with open(hostFile) as f:
+        ips = f.read().splitlines()
+    return ips
+ 
+def writeSaltMasterEntry(rosterFile, iphost):
+    rosterFile.write(" salt-master:" + '\n')
+    rosterFile.write("   user: root" + '\n')
+    rosterFile.write("   host: " + str(iphost) + '\n')
+
+
+def writeSaltMinionEntry(rosterFile, iphost):
+    rosterFile.write(" salt-minion:" + '\n')
+    rosterFile.write("   user: root" + '\n')
+    rosterFile.write("   host: " + str(iphost) + '\n')
+
+# read terraform ips
+if os.path.exists(roster_file):
+    os.remove(roster_file)
+
+ips = readIpfromTerraform(terraformHostsFile)
+if not ips:
+  print("host.txt empty")
+  sys.exit(1)
+
+# create dinamic roster
 with open(roster_file, 'a') as roster:
     # take first as master
-    writeSaltMasterEntry(roster, ips.pop)
+    writeSaltMasterEntry(roster, ips.pop())
     for ip in ips:
         writeSaltMinionEntry(roster, ip)
-def writeSaltMasterEntry(rosterFile, ip):
-    rosterFile.write(" salt-master:" + '\n')
-    rosterFile.write("   user: root" + ip + '\n')
-    rosterFile.write("   host:" + ip + '\n')

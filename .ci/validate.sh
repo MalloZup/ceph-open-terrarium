23298b559aa3bcb7dbafc0fa9e7d14f606e21ef9
dmaiocchi <dmaiocchi@suse.com>
2018-10-11 23:13:38 +0200

    install pkg

--- .ci/validate.sh
+++ .ci/validate.sh
@@ -3,6 +3,7 @@
 # we call this script inside the .ci dir
 
 set -e
+export LIBVIRT_DEFAULT_URI="qemu:///system"
 
 cd ..
 if [ `terraform fmt | wc -c` -ne 0 ]; then echo "terraform files need be formatted! run terraform fmt!"; exit 1; fi
